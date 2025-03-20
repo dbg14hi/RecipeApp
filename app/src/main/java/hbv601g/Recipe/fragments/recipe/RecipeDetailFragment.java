6fc8a6d9a095eeffb82a52525d77fe7885b57ae3
arnakr <aks2@hi.is>
2025-03-20 14:26:34 +0000

    Arna - update password og email

--- app/src/main/java/hbv601g/Recipe/fragments/recipe/RecipeDetailFragment.java
+++ app/src/main/java/hbv601g/Recipe/fragments/recipe/RecipeDetailFragment.java
@@ -26,6 +26,7 @@ import android.widget.Toast;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
